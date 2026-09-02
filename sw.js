ceaf32664c665b57ee41e30db21e28d4477d1e84
ThunderPunch <215190383@qq.com>
2026-09-02 20:38:37 +0800

    Refresh cache for tablet layout fix

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const CACHE_NAME="baby-growth-pwa-v1.1.5-consumer-ui";
+const CACHE_NAME="baby-growth-pwa-v1.1.6-tablet-layout";
 const APP_SHELL=[
-  "./","./index.html","./styles.css","./styles-base.css","./app.js","./export-ipad.js","./profile-save-guard.js","./db.js","./manifest.webmanifest",
+  "./","./index.html","./styles.css","./styles-base.css","./layout-fix.css","./app.js","./export-ipad.js","./profile-save-guard.js","./db.js","./manifest.webmanifest",
   "./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"
 ];
 
