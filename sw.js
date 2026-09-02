eb7d3ab7e0c38200e5dae3913f57582e5de0a136
ThunderPunch <215190383@qq.com>
2026-09-02 18:08:07 +0800

    Refresh cache for fixed title bar

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="baby-growth-pwa-v1.1.1-date-nav-fixed";
+const CACHE_NAME="baby-growth-pwa-v1.1.2-topbar-fixed";
 const APP_SHELL=[
   "./","./index.html","./styles.css","./styles-base.css","./app.js","./db.js","./manifest.webmanifest",
   "./icons/icon-192.png","./icons/icon-512.png","./icons/apple-touch-icon.png"
