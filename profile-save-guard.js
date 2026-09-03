7af34a74846c0a2095b80fcd713071aed8314882
ThunderPunch <215190383@qq.com>
2026-09-03 09:02:17 +0800

    Track long-term profile fields as unsaved changes

--- profile-save-guard.js
+++ profile-save-guard.js
@@ -2,6 +2,7 @@ const PROFILE_FIELD_SELECTOR = [
   "#babyName", "#birthDate", "#sex",
   "#weekdayBedtime", "#weekdayLatency", "#weekdayNaps", "#weekdayCaregiver",
   "#weekendBedtime", "#weekendLatency", "#weekendNaps", "#weekendCaregiver",
+  "#feedingMode", "#sleepEnvironment", "#settlingMethod",
   "#mainIssue"
 ].join(",");
 
