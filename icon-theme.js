e86889f4624bb5108bf4353111337ef1ce8db3d4
ThunderPunch <215190383@qq.com>
2026-09-03 11:33:04 +0800

    Refine baby profile tab silhouette

--- icon-theme.js
+++ icon-theme.js
@@ -21,10 +21,12 @@ function iconUrl(variant){
 function manifestUrl(variant){ return variant==="neutral" ? "./manifest.webmanifest" : `./manifest-${variant}.webmanifest`; }
 
 function profileIconMarkup(variant){
-  const base='<path d="M12 5.2c-2.9 0-4.9 2.1-5.1 5.3-1.2-.3-2.1.5-2.1 1.7 0 1.3 1 2.1 2.3 1.7.7 3 2.4 4.7 4.9 4.7s4.2-1.7 4.9-4.7c1.3.4 2.3-.4 2.3-1.7 0-1.2-.9-2-2.1-1.7-.2-3.2-2.2-5.3-5.1-5.3Z"/>';
-  const girl='<path d="M14.7 5.7c-.8-.9-1.7-1.2-2.2-.6-.5.6-.2 1.5.8 2.1l1.4-1.5Zm.3.1c.9-.8 1.9-.9 2.3-.2.4.7 0 1.5-1 1.9L15 5.8Z"/><circle cx="14.9" cy="6" r=".65"/>';
-  const boy='<path d="M10.9 5.3c.2-1.4 1.4-2.3 2.5-1.7 1 .5 1.1 1.8.3 2.4-.6.5-1.6.3-1.8-.4"/>';
-  return base+(variant==="girl"?girl:variant==="boy"?boy:"");
+  // Chubby cranium, small lower face and softly protruding ears read more clearly as a baby at 24px.
+  const base='<path d="M12 5.05c-3.05 0-5.12 2.25-5.35 5.48-1.34-.35-2.42.54-2.42 1.88 0 1.42 1.16 2.34 2.58 1.82.7 2.95 2.6 4.62 5.19 4.62s4.49-1.67 5.19-4.62c1.42.52 2.58-.4 2.58-1.82 0-1.34-1.08-2.23-2.42-1.88C17.12 7.3 15.05 5.05 12 5.05Z"/>';
+  const neutral='<path d="M11.15 5.18c-.4-.78-.08-1.63.73-1.95.86-.34 1.67.2 1.68.98.01.72-.59 1.2-1.2 1.11"/>';
+  const girl='<path d="M14.25 5.2c-.72-.86-1.65-1.09-2.14-.48-.48.6-.1 1.44.86 1.92l1.28-1.44Zm.42.12c.94-.73 1.94-.72 2.28.02.34.75-.19 1.47-1.18 1.79l-1.1-1.81Z"/><circle cx="14.48" cy="5.46" r=".58"/>';
+  const boy='<path d="M10.85 5.15c-.34-1.02.34-2.05 1.4-2.12 1.12-.07 1.87.96 1.46 1.85-.35.76-1.42 1.02-2 .37"/>';
+  return base+(variant==="girl"?girl:variant==="boy"?boy:neutral);
 }
 
 function ensureStyle(){
